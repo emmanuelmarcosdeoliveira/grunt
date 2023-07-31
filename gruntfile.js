a6bd184974991eb8430d4d45fb456258e3a59f16
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2023-07-31 11:39:32 -0300

    :construction: Comprimindo HTMl com Grunt

--- gruntfile.js
+++ gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function (grunt) {
     less: {
       development: {
         files: {
-          "main.css": "main.less",
+          "dev/styles/main.css": "src/styles/main.less",
         },
       },
       production: {
@@ -12,34 +12,81 @@ module.exports = function (grunt) {
           compress: true,
         },
         files: {
-          "main.min.css": "main.less",
+          "dist/styles/main.min.css": "src/styles/main.less",
         },
       },
     },
-    sass: {
+    watch: {
+      less: {
+        files: ["src/styles/**/*.less"],
+        tasks: ["less:development"],
+      },
+      html: {
+        files: ["src/index.html"],
+        tasks: ["replace:dev"],
+      },
+    },
+    replace: {
+      dev: {
+        options: {
+          patterns: [
+            {
+              match: "ENDERECO_DO_CSS",
+              replacement: "./styles/main.css",
+            },
+          ],
+        },
+        files: [
+          {
+            expand: true,
+            flatten: true,
+            src: ["src/index.html"],
+            dest: "dev/",
+          },
+        ],
+      },
       dist: {
         options: {
-          style: "compressed",
+          patterns: [
+            {
+              match: "ENDERECO_DO_CSS",
+              replacement: "./styles/main.min.css",
+            },
+          ],
+        },
+        files: [
+          {
+            expand: true,
+            flatten: true,
+            src: ["prebuild/index.html"],
+            dest: "dist/",
+          },
+        ],
+      },
+    },
+    htmlmin: {
+      dist: {
+        options: {
+          removeComments: true,
+          collapseWhitespace: true,
         },
         files: {
-          "main2.css": "main.scss",
+          "prebuild/index.html": "src/index.html",
         },
       },
     },
-    concurrent: {
-      target: ["dev-oliveira", "less", "sass"],
-    },
+    clean: ["prebuild"],
   });
-  grunt.registerTask("dev-oliveira", function () {
-    const done = this.async();
-    setTimeout(function () {
-      console.log("Olá Dev-Oliveira vamos testar o Grunt");
-      done();
-    }, 4000);
-  });
-
   grunt.loadNpmTasks("grunt-contrib-less");
-  grunt.loadNpmTasks("grunt-contrib-sass");
-  grunt.loadNpmTasks("grunt-concurrent");
-  grunt.registerTask("default", ["concurrent"]);
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-replace");
+  grunt.loadNpmTasks("grunt-contrib-htmlmin");
+  grunt.loadNpmTasks("grunt-contrib-clean");
+  grunt.registerTask("default", ["watch"]);
+  grunt.registerTask("build", [
+    "less:production",
+    "htmlmin:dist",
+    "replace:dist",
+    "clean",
+  ]);
 };
